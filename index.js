299edbf59eb8e0d188d6d5c0f4b877779b12eb83
GuiltyGamingJay <jerontai07@gmail.com>
2022-11-17 13:10:17 -0500

    constants written for team members

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //node module
 const inquirer = require("inquirer");
 const fs = require("fs");
+//constants for team members
+const manager = require("./library/manager");
+const engineer = require("./library/engineer");
+const intern = require("./library/intern");
+const employee = require("./library/employee");
 
-//Team profiles
-const 
+const generateHtml = require("./Src/htmlTemplate");
+const {rejects} = require("assert");
+//array for all team members
+const teamARR = [];
+
